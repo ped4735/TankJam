60e91d52ab927bc881c5adaf421241bcb7faacae
LSchoninger <33501063+LSchoninger@users.noreply.github.com>
2018-09-28 21:26:02 -0300

    First COmmit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+	public float velocity;
+	public float range;
+	private float contador;
+
+
+
+	void Start ()
+	{
+		contador = 0;
+	}
+	void Update ()
+	{
+		if(range>=contador)
+		{
+			transform.position += (transform.up * (velocity * Time.deltaTime));
+			contador += Time.deltaTime;
+			print(contador);
+		}
+		else
+		{
+			transform.rotation=Quaternion.Euler(new Vector3(transform.rotation.x,transform.rotation.y,transform.rotation.z+180));
+			contador = 0;
+
+		}
+	}
+}
